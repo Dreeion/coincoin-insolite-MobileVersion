f31cd74453996b30a219c83b570028f0bad648e9
Dreeion <a.garault44@gmail.com>
2020-04-28 12:51:31 +0200

    TECH# Restructuration de l'app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,31 +2,32 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home',
-   loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: '', redirectTo: 'pages/home', pathMatch: 'full' },
+  { path: 'pages/home',
+   loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
   {
     path: 'settings',
-    loadChildren: () => import('./settings/settings.module').then( m => m.SettingsPageModule)
+    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
   },
   {
     path: 'galerie',
-    loadChildren: () => import('./galerie/galerie.module').then( m => m.GaleriePageModule)
+    loadChildren: () => import('./pages/galerie/galerie.module').then( m => m.GaleriePageModule)
   },
   {
     path: 'carte',
-    loadChildren: () => import('./carte/carte.module').then( m => m.CartePageModule)
+    loadChildren: () => import('./pages/carte/carte.module').then( m => m.CartePageModule)
   },
   {
     path: 'cgu',
-    loadChildren: () => import('./cgu/cgu.module').then( m => m.CGUPageModule)
-  },  {
+    loadChildren: () => import('./pages/cgu/cgu.module').then( m => m.CGUPageModule)
+  },
+  {
     path: 'inscription',
-    loadChildren: () => import('./inscription/inscription.module').then( m => m.InscriptionPageModule)
+    loadChildren: () => import('./pages/inscription/inscription.module').then( m => m.InscriptionPageModule)
   },
   {
     path: 'mdp-perdu',
-    loadChildren: () => import('./mdp-perdu/mdp-perdu.module').then( m => m.MdpPerduPageModule)
+    loadChildren: () => import('./pages/mdp-perdu/mdp-perdu.module').then( m => m.MdpPerduPageModule)
   }
 
 
