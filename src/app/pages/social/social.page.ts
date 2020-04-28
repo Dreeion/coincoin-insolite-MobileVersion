d170a0413b666e1c3d8c7fde5de8def170905e44
camillenaulet03 <camille.naulet@hotmail.fr>
2020-04-28 16:48:13 +0200

    Ajouté: récupération des images dans firebase

--- src/app/pages/social/social.page.ts
+++ src/app/pages/social/social.page.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireDatabase, AngularFireDatabaseModule} from '@angular/fire/database';
+import { AngularFireStorage } from '@angular/fire/storage';
 
 @Component({
   selector: 'app-social',
   templateUrl: './social.page.html',
   styleUrls: ['./social.page.scss'],
 })
-export class SocialPage implements OnInit {
+export class SocialPage {
 
-  constructor() { }
+  images = [];
 
-  ngOnInit() {
+  constructor(
+      public afDB: AngularFireDatabase,
+      public afSG: AngularFireStorage
+  ) {
+    this.getImagesDatabase();
+  }
+
+  getImagesDatabase() {
+    this.afDB.list('Images/').snapshotChanges(['child_added']).subscribe(images => {
+      images.forEach(image => {
+        this.getImagesStorage(image);
+      });
+    });
+  }
+
+  getImagesStorage(image: any) {
+    const imgRef = image.payload.exportVal().ref;
+    this.afSG.ref(imgRef).getDownloadURL().subscribe(imgUrl => {
+      this.images.push({
+        name: image.payload.exportVal().name,
+        url: imgUrl
+      })
+;    });
   }
 
 }
