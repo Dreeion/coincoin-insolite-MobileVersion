2b9e99a5ac7e8a589260302bee7eaf82127a6cfc
Dreeion <a.garault44@gmail.com>
2020-04-30 05:10:08 +0200

    FIX# Création du service User.

---
+++ src/app/services/userService/user-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { FirebaseService } from '../firebaseService/firebase-service.service'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  constructor(){
+  }
+
+}
