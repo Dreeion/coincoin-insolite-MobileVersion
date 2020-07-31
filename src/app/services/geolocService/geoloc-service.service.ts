e37c5213f55d74ccd82254665da027ffb1ccf9c6
Johan DELEON <johan@doowup.fr>
2020-07-31 10:24:44 +0200

    IMPROVE : Geolocalisation

--- src/app/services/geolocService/geoloc-service.service.ts
+++ src/app/services/geolocService/geoloc-service.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GeolocServiceService {
+export class GeolocService {
 
-  constructor() { }
+  constructor(
+    private geolocation: Geolocation
+  ) { }
+
+  currentPosition() : Promise<any>{
+    return new Promise(resolve => { 
+    this.geolocation.getCurrentPosition().then((resp) => {
+      var data = {
+        lat : resp.coords.latitude,
+        long : resp.coords.longitude
+      }
+      resolve(data)
+     }).catch((error) => {
+       console.log('Error getting location', error);
+     });
+    })
+  }
+
 }
